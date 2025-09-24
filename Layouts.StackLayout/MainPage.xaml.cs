4f320cb476b4b1a268ed0710fa8e2df593bd5fbf
Abraham Samuel Coot Ku <23470124@campeche.tecnm.mx>
2025-09-24 12:49:07 -0600

    Agrega el ejemplos del uso de StackLayout

---
+++ Layouts.StackLayout/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Layouts.StackLayout
+{
+    public partial class MainPage : ContentPage
+    {
+      
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+    }
+}
